571f0a4afdc0e09b369c34d863f0ffa4f2ae60f9
Sydney Tsai <90071536+sydneytsai@users.noreply.github.com>
2022-10-03 13:34:02 -0700

    Delete hw6 directory

--- hw6/README.md
+++
@@ -1,3 +0,0 @@
-# hw6
-
-Template for HW6 of Cubstart (CS 198-75).
--- hw6/client/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function Header() {
-    return (
-        <header>
-            Cubstart Video
-        </header>
-    );
-}
-export default Header;
