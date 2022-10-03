beaef183ff3a46772763cf25e7adea896ca2d6bf
Sydney Tsai <90071536+sydneytsai@users.noreply.github.com>
2022-10-03 13:24:25 -0700

    Add files via upload

---
+++ hw5/src/PathfindingVisualizer/Node/Node.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Node.css";
+
+// BEGIN PART 3
+
+const Node = (/* YOUR CODE HERE */) => {
+    col,
+    isFinish,
+    isStart,
+    isWall,
+    onMouseDown,
+    onMouseEnter,
+    onMouseUp,
+    row,
+  return <div /* YOUR CODE HERE */></div>;
+};
+
+
+// END PART 3
+
+export default Node;
